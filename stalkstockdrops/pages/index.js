3730777137ee980a19abbb2f9369d06f5ad328a7
Ignacio Garcia <69227346+Agarc4777@users.noreply.github.com>
2021-03-30 20:15:38 -0500

    added comments to index.js

--- stalkstockdrops/pages/index.js
+++ stalkstockdrops/pages/index.js
@@ -5,8 +5,8 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>StockDrop</title>
+        <link rel="" href="/" /> 
       </Head>
 
       <main className={styles.main}>
