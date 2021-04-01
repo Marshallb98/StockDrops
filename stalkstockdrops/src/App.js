e8887105a791cb38b8231990b0fa258c64ba04bd
Marshallb98 <69322012+Marshallb98@users.noreply.github.com>
2021-04-01 18:35:54 -0500

    added first crawler

--- stalkstockdrops/src/App.js
+++ stalkstockdrops/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Main from "./components/Main/Main";
-
+import Feed from "./components/Feed"
 function App() {
   return (
     <div>
       <Main />
+      <Feed/>
     </div>
   )  
 }
