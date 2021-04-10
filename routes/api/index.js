1121eeb6ccfb92bb068d889cd6c547d3d5141eb1
Ignacio Garcia <69227346+Agarc4777@users.noreply.github.com>
2021-04-10 12:21:16 -0500

    deleted everything to pull from main for a working local host application

--- routes/api/index.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const itemRoutes = require("./items");
-const usersRoutes = require("./users");
-router.use("/user", usersRoutes);
-router.use("/items", itemRoutes);
-
-module.exports = router;
--- routes/api/items.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const itemsController = require("../../controllers/itemsController");
-
-router.route("/")
-  .get(itemsController.findAll)
-
-
-module.exports = router;
